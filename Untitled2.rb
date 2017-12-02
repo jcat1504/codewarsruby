05083792ba1191f0dd6495e851a1e85f311d23ba
Jess Y <31145424+jcat1504@users.noreply.github.com>
2017-12-02 14:03:55 -0800

    Add files via upload

---
+++ Untitled2.rb
@@ -0,0 +1,7 @@
+#Make a program that filters a list of strings and returns a list with only
+#your friends name in it(4letters only!)
+
+def friend(friends)
+  friends = ["Ryan", "Kieran", "Jason", "Mark"]
+  friends.select {|friends| put friends if friends.length = 4}
+end
