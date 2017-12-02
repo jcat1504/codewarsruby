eeed6cbd90f667bd146cce9ff88441bc9d46afbb
Jess Y <31145424+jcat1504@users.noreply.github.com>
2017-12-02 10:50:27 -0800

    Add files via upload

---
+++ countthechars.rb
@@ -0,0 +1,8 @@
+#The goal of this kata is to write a function that takes
+#two inputs: a string and a character. The function will count the
+#number of times that character appears in the string.
+#The count is case insensitive.
+
+def count_char(string, char)
+  string.downcase.chars.count(char.downcase)
+end
