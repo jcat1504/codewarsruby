c81d59ce81ae1855b16f618e8c4710f94ec94063
Jess Y <31145424+jcat1504@users.noreply.github.com>
2017-12-13 17:44:50 -0800

    Add files via upload

---
+++ Untitled6.rb
@@ -0,0 +1,9 @@
+#Take an integer n(n >= 0) and a digit d(0 <= d <=9) as an integer.
+#Square all numbers k(0 <= k <= n) between 0 and n.
+#Count the number of digits d used in the writing of all the k**2
+#call nb_dig(or nbDig or..) the function taking n and d as parameters
+#and returning this count
+
+def nb_dig(n, d)
+  (0..n).map {|k| k**2}.join.count d.to_s
+end
