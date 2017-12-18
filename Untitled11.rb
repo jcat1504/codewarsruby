f6018bf94bdb0772f6b7ccaebb8b82166b3334e7
Jess Y <31145424+jcat1504@users.noreply.github.com>
2017-12-18 08:30:09 -0800

    Update Untitled11.rb

--- Untitled11.rb
+++ Untitled11.rb
@@ -1,3 +1,8 @@
 #Given a string, reverse it omitting all non-alphabetic letters
+def reverse_letter(string)
 return string.reverse.delete("53?21")
 end
+
+def reverse_letter(string)
+  string.delete("^A-Za-z").reverse
+end 
