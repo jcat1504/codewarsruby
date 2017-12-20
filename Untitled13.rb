d423ff1986bc6adadf73094dd61dba3a43033b47
Jess Y <31145424+jcat1504@users.noreply.github.com>
2017-12-20 15:17:23 -0800

    Add files via upload

---
+++ Untitled13.rb
@@ -0,0 +1,6 @@
+#Find the number with the most digits. If two numbers in the argument
+#array have same number of digits, return first one in array.
+
+def find_longest(arr)
+  arr.max_by{|num| num.to_size}
+end
