63317bbfbe8166e15a6dcb62d640ac0e1deea35d
Jess Y <31145424+jcat1504@users.noreply.github.com>
2017-12-22 11:10:14 -0800

    Add files via upload

---
+++ Untitled15.rb
@@ -0,0 +1,7 @@
+#Write a function called autocorrect that takes a string and
+#replaces all instances of "you" or "u", with "your sister"
+
+def auto_correct(input)
+  input.gsub!(/\b(u|you|you(u*))\b/i,"your sister")
+return input
+end
