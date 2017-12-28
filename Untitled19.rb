2a78e2dd0a806b434da463e9b5a08d54e0e53a3f
Jess Y <31145424+jcat1504@users.noreply.github.com>
2017-12-27 20:45:13 -0800

    Update Untitled19.rb

--- Untitled19.rb
+++ Untitled19.rb
@@ -7,3 +7,9 @@
 def is_sorted_and_how(arr)
   arr == arr.sort? 'yes, ascending' : arr == arr.sort.reverse? 'yes,descending' : 'no'
 end
+
+def is_sorted_and_how(arr)
+  return 'yes, ascending' if arr == arr.sort
+  return 'yes, descending' if arr == arr.sort.reverse
+  "no"
+end 
