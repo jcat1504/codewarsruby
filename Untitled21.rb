b5c986e29fa1bff704e6c7b3fea3736f6c1e5cb2
Jess Y <31145424+jcat1504@users.noreply.github.com>
2017-12-29 09:22:40 -0800

    Add files via upload

---
+++ Untitled21.rb
@@ -0,0 +1,10 @@
+#given an array of integers, remove the smallest value. do not mutate the
+#original array/list. If there are multiple items/elements with the same
+#value, remove the one with the lower index.
+#If you get an empty array/list, return the empty array/list
+
+def remove_smallest(numbers)
+  return [] if numbers.empty?
+  numbers.delete_at(numbers.index(numbers.min))
+  return numbers
+end 
