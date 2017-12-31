0712d5c474b67b87a43524bba3cdca06bf863b58
Jess Y <31145424+jcat1504@users.noreply.github.com>
2017-12-30 17:54:08 -0800

    Add files via upload

---
+++ Untitled22.rb
@@ -0,0 +1,7 @@
+#An isogram is a word that has no repeating letters, consecutive or non-consecutive.
+#Implement a function that determines whether a string that contains only
+#letters is an isogram
+
+def is_isogram(string)
+string.downcase.chars.uniq == string.downcase.chars
+end
