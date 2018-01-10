e8c851faa6d50e3c041b74f5161ea19ee11d0d3c
Jess Y <31145424+jcat1504@users.noreply.github.com>
2018-01-09 21:09:13 -0800

    Add files via upload

---
+++ listfiltering.rb
@@ -0,0 +1,5 @@
+#In this kata, you will create a function that takes a list of non-negative
+#integers and strings and returns a new list with strings filtered out
+def filter_list(l)
+  l.reject{|x| x.is_a? string}
+end
