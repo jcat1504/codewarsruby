fe8ca7a6559ca2833ba5c443adf2d77506c95021
Jess Y <31145424+jcat1504@users.noreply.github.com>
2018-01-11 10:17:52 -0800

    Add files via upload

---
+++ averagescores.rb
@@ -0,0 +1,5 @@
+#Create a function that returns an average of an array of numbers("scores"),
+r#ounded to the nearest whole number. You are not allowed to use any loops
+
+array.inject(:+).fdiv(array.length).round
+end
