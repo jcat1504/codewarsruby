ca624755efe636a639de55f4f138444a9c6c8d36
Jess Y <31145424+jcat1504@users.noreply.github.com>
2018-01-12 18:50:34 -0800

    Update averagescores.rb

--- averagescores.rb
+++ averagescores.rb
@@ -4,3 +4,7 @@ r#ounded to the nearest whole number. You are not allowed to use any loops
 array.inject(:+).fdiv(array.length).round
 end
 
+
+def average(scores)
+  scores.reduce(:+).fdiv(scores.count).round 
+end 
