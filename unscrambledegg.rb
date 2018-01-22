0a566cd7ad491d0bff1ee67131a65d109451f808
Jess Y <31145424+jcat1504@users.noreply.github.com>
2018-01-21 18:37:06 -0800

    Add files via upload

---
+++ unscrambledegg.rb
@@ -0,0 +1,6 @@
+#The string given to your function has had an egg inserted directly under
+#each consonant. You need to return the string before it becomes eggcoded
+
+def unscramble_eggs(word)
+  word.gsub(/egg/, " ")
+end 
