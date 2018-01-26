4a66da4f8aaf195e879a2b9de525dd7e0ea6c81f
Jess Y <31145424+jcat1504@users.noreply.github.com>
2018-01-25 23:34:47 -0800

    Add files via upload

---
+++ arewealternate.rb
@@ -0,0 +1,4 @@
+#Create a function isAlt that accepts a string as an argument and validates
+#whether the vowels(aeiou) and consonants are in alternate order
+
+def isAlt(s) ! (/[aeiou]{2}/ =~s) end
