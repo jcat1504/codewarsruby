81dd3e357061407da51e1c3e23a2e0014a53ca6c
Jess Y <31145424+jcat1504@users.noreply.github.com>
2018-01-27 08:13:23 -0800

    Add files via upload

---
+++ createphonenumber.rb
@@ -0,0 +1,6 @@
+#Write a function that accepts an array of 10 integers, that returns a
+#String of those numbers in the form of a phone number
+
+def createPhoneNumber
+  '(%d%d%d) %d%d%d - %d%d%d%d' % array end
+  
