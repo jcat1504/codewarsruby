b2873699b7fb76946cd52d6224c5bab249562091
Jess Y <31145424+jcat1504@users.noreply.github.com>
2018-01-29 08:47:21 -0800

    Add files via upload

---
+++ validphonenumber.rb
@@ -0,0 +1,8 @@
+#Write a function that accepts a string and returns true if it is in
+#the form of a phone number
+#Assume that any integer from 0 - 9 in any of the spots will produce a
+#valid phone number
+
+def valdPhoneNumber(phonenumber)
+  !! phone number [/\A\(\d{3}\)\s\d{3}-\d{4}\z/] end
+  
