ab079f638c34ab3991c0a84b8925c59b3ca7eb4f
Jess Y <31145424+jcat1504@users.noreply.github.com>
2018-02-06 20:10:43 -0800

    Add files via upload

---
+++ ampersandnames.rb
@@ -0,0 +1,10 @@
+#Format a string of names like 'Bart, Lisa & Maggie'
+#Given: an array containing hashes of names
+#Return: a string formatted as a list of names separated by commas, except
+#for the last two names, which should be separated by an ampersand
+
+def list_names
+  names = names.map{|name| name[:name]}
+  last_name = names.pop
+  return last_name.to_s if names.empty?
+  "#{names.join(' , ')}& #{last_name}" end 
