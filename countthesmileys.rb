82aa86c2f764824b6d8999215c48e5cf2233b9ff
Jess Y <31145424+jcat1504@users.noreply.github.com>
2018-02-08 19:10:34 -0800

    Add files via upload

---
+++ countthesmileys.rb
@@ -0,0 +1,6 @@
+#Given an array(arr)as an argument complete the function countSmileys that
+#should return number of smiley faces
+
+def count_smileys(arr)
+  arr.count{|e| e=~ / (:|;) {1}(-|~)?(\)/D/}end
+  
