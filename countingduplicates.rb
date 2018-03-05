7f9dfc7f76c65ed86e75ecc0ede7e459d26aadcb
Jess Y <31145424+jcat1504@users.noreply.github.com>
2018-03-05 13:46:11 -0800

    Update countingduplicates.rb

--- countingduplicates.rb
+++ countingduplicates.rb
@@ -4,3 +4,6 @@
 def duplicate_count(text)
   ('a'..'z').count {|c| text.downcase.count(c) > 1} end
   
+def duplicate_count(text)
+  arr = text.downcase.split(" ")
+  arr.uniq.count{|n| arr.count(n) > 1} end 
