3838da66274ce849cbf902b7231dedd3bc728d96
Jess Y <31145424+jcat1504@users.noreply.github.com>
2018-03-22 20:41:59 -0700

    Update findpartyoutlier.rb

--- findpartyoutlier.rb
+++ findpartyoutlier.rb
@@ -4,3 +4,6 @@
 
 def find_outlier(integers)
   integers.partition(&:odd?).find(&:one?).first end 
+
+def find_outlier(integers)
+  outlier = integers.first(3).count(&: even?)<2 ? :even ? :odd? integers.find(&outlier) end 
