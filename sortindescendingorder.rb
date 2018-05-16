87d0706f3e3c136e2afe6b398dba76c80155475c
Jess Y <31145424+jcat1504@users.noreply.github.com>
2018-05-16 08:43:05 -0700

    Add files via upload

---
+++ sortindescendingorder.rb
@@ -0,0 +1,13 @@
+#You have to sort the inner content of every word of a string in descending order.
+#The inner content is the content of a word without first and the last char.
+
+#Some examples:
+
+#"sort the inner content in descending order" -> "srot the inner ctonnet in
+#dsnnieedcg oredr"
+#"wait for me" -> "wiat for me"
+#"this kata is easy" -> "tihs ktaa is esay"
+
+def sort_the_inner_content(words)
+  words.split.each { |x| x[1..-2] = x[1..-2].chars.sort.reverse.join }.join(' ')
+end
