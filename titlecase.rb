b43e662fb0925cdd2fd337a7ba03f4f65b5d9ea6
Jess Y <31145424+jcat1504@users.noreply.github.com>
2018-05-21 18:54:46 -0700

    Update titlecase.rb

--- titlecase.rb
+++ titlecase.rb
@@ -9,3 +9,8 @@ if the case of the minor word string is changed.
   def title_case(title, minor_words = '')
     title.capitalize.split().map{|a| minor_words.downcase.split().include?(a) ? a : a.capitalize}.join(' ')
   end
+
+  def title_case(title, minor_words='')
+  minor_words = minor_words.downcase.split(' ')
+  title.capitalize.split(' ').map { |word| minor_words.include?(word.downcase) ? word : word.capitalize }.join(' ')
+end
