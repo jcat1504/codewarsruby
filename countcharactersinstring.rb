8aaf19e6290d605e105e1c84c46624961502efb1
Jess Y <31145424+jcat1504@users.noreply.github.com>
2018-05-24 09:06:31 -0700

    Update countcharactersinstring.rb

--- countcharactersinstring.rb
+++ countcharactersinstring.rb
@@ -6,3 +6,13 @@ What if the string is empty ? Then the result should be empty object literal { }
 def count_chars(s)
   s.chars.uniq.each_with_object({}) { |c, h| h[c] = s.count(c) }
 end
+
+  def count_chars(s)
+  return {} if s.empty?;
+  counts = Hash.new;
+  s.split("").each { |i|
+    counts[i] = 0 if counts[i] == nil
+    counts[i] += 1;
+  };
+  return counts;
+end
